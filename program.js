9f4f80399729e4a6545b1edac3b052f064c9752b
DuyJohannes <duy.hpk0870@sinhvien.hoasen.edu.vn>
2021-01-28 11:07:43 +0700

    sua3

--- program.js
+++ program.js
@@ -1,46 +1,53 @@
-function isPrimeNumber(n) {
-    var flag = true;
-    if (n < 2){
+function Ktsnt(n) {
+  let flag = true;
+  if (n < 2) {
+    flag = false;
+  }
+  else {
+    for (let i = 2; i < n - 1; i++) {
+      if (n % i == 0) {
         flag = false;
-    }
-    else{
-        for (var i = 2; i < n-1; i++)
-        {
-            if (n % i == 0){
-                flag = false;
-                break;
-            }
-        }
-    }
-    return flag;
-}
-
-
-function ketqua(input) {
-  let danhsachketqua = [];
-  let currentV = input;
-  while (!isPrimeNumber(currentV)) {
-    for (let i = 0; i < currentV; i++) {
-      if (isPrimeNumber(i) && currentV % i == 0) {
-        danhsachketqua.push(i);
-        currentV = currentV / i;
         break;
       }
     }
   }
-  danhsachketqua.push(currentV);
-  return danhsachketqua;
+  return flag;
+}
+
+function ketqua(n) {
+  let danhsachketqua = [];
+  let sohientai = n;
+  while (!Ktsnt(sohientai)) {
+    for (let i = 0; i < sohientai; i++) {
+      if (Ktsnt(i) && sohientai % i == 0) {
+        danhsachketqua.push(i);
+        sohientai = sohientai / i;
+        break;
+      }
+    }
+  }
+  danhsachketqua.push(sohientai);
+  let print = "";
+  for (let i = 0; i < danhsachketqua.length; i++) {
+    if (i == 0) {
+      print += danhsachketqua[i];
+    } else {
+      print += " " + danhsachketqua[i];
+    }
+  }
+  return print;
 }
 
 function main(input) {
   let res = input.split(" ");
-  let n = parseInt(res[0]);
-  let kq = ketqua(n);
-  let print=" ";
-  for (let i = 0; i < kq.length; i++) {
-    print=print+" "+kq[i];
+  // let n = parseInt(res[0]);
+  // let kq = ketqua(n);
+  let n = parseInt(res)
+  if (n < 2) {
+    console.log(n);
+  } else {
+    console.log(ketqua(n));
   }
-  console.log(print);
-  
 }
 module.exports = main;
+
